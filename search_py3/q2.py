4ed55e65d853ada9999ee04747a50909a7735308
eugenechia95 <eugenechia95@gmail.com>
2020-06-04 00:44:15 +0800

    done with hw

--- search_py3/q2.py
+++ search_py3/q2.py
@@ -39,10 +39,48 @@ class FlightProblem(Problem):
   def value(self, state):
     return None
 
-
-
 def find_itinerary(start_city, start_time, end_city, deadline):
   flight_problem = FlightProblem((start_city, start_time),(end_city, deadline))
-  return breadth_first_tree_search(flight_problem).solution()
+  try:
+    return breadth_first_tree_search(flight_problem).solution()
+  except:
+    return None
 
-print(find_itinerary('Rome', 1, 'Paris', 8))
+#Yes, it will find the path the soonest. This is similar to uniform cost search. We will get an optimal solution
+
+
+# print(find_itinerary('Rome', 1, 'Paris', 7))
+
+def find_shortest_itinerary(start_city, end_city):
+  idx = 1
+  times_called = 0
+  path = find_itinerary(start_city, 1, end_city, idx)
+  times_called += 1
+  while path == None:
+    idx += 1
+    path = find_itinerary(start_city, 1, end_city, idx)
+    times_called += 1
+  print(times_called)
+  return path
+
+print(find_shortest_itinerary('Paris', 'Constantinople'))
+
+# Jumps in increments of 3, upon finding, decrease deadline until no path can be found
+def find_shortest_itinerary_improved(start_city, end_city):
+  idx = 1
+  times_called = 0
+  path = find_itinerary(start_city, 1, end_city, idx)
+  times_called += 1
+  while path == None:
+    idx += 3
+    path = find_itinerary(start_city, 1, end_city, idx)
+    times_called += 1
+  while path != None:
+    idx -= 1
+    final_path = path
+    path = find_itinerary(start_city, 1, end_city, idx)
+    times_called += 1
+  print(times_called)
+  return final_path
+
+print(find_shortest_itinerary_improved('Paris', 'Constantinople'))
